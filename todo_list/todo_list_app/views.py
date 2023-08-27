9cb758c0d64ad4b6f7af44f97c4ecd9f97c09b96
darienMoltaji <d.moltaji@gmail.com>
2023-08-26 19:01:51 -0700

    Create base todo app

---
+++ todo_list/todo_list_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+# TODO Create a view that serves the todolist html page
+# TODO Create a view to add to the database 
+# TODO Create a view to remove from the database 
+# TODO Create a view to edit TodoItem value
+# TODO Create a view to check/uncheck TodoItem
+# TODO models for all these
